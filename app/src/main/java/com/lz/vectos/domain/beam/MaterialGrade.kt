bea144e30acba8a149607d85df544126b50f1c9b
Haptonstall <41123387+Haptonstall@users.noreply.github.com>
2026-05-27 14:24:22 -0400

    Compat: add typealias for MaterialGrade to maintain package compatibility

---
+++ app/src/main/java/com/lz/vectos/domain/beam/MaterialGrade.kt
@@ -0,0 +1,4 @@
+package com.lz.vectos.domain.beam
+
+@Deprecated("Use com.lz.vectos.domain.structural.MaterialGrade instead")
+typealias MaterialGrade = com.lz.vectos.domain.structural.MaterialGrade
